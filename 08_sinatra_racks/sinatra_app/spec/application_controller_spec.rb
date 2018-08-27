74b15fdf4d88cd4525ebe721f9c37515c2892e8b
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-08-27 11:00:53 -0400

    Update to include info

--- 08_sinatra_racks/sinatra_app/spec/application_controller_spec.rb
+++ 08_sinatra_racks/sinatra_app/spec/application_controller_spec.rb
@@ -1,7 +1,7 @@
 require_relative './spec_helper'
 
 describe "Basic Routes" do
-  describe "GET '/books'" do
+  describe "Index Action - GET '/books'" do
     it 'returns a status code of 200' do
       get '/books'
       expect(last_response.status).to eq(200)
@@ -9,7 +9,31 @@ describe "Basic Routes" do
 
     it 'returns the index.erb' do
       get '/books'
-      expect(last_response.body).to include("<h1>All Books</h1>")
+      expect(last_response.body).to include("All Books")
+    end
+
+    it 'the template contains all books in the database' do
+      @book = Book.last
+
+      get '/books'
+      expect(last_response.body).to include(@book.title)
     end
   end
+
+  describe "Show Action - GET '/books/:id'" do
+
+    it 'returns a status code of 200' do
+      @book = Book.first
+      get "/books/#{@book.id}"
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'returns the show.erb' do
+      @book = Book.first
+      get "/books/#{@book.id}"
+
+      expect(last_response.body).to include("Book Details")
+    end
+  end
+
 end
